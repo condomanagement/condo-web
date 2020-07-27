fd6582f52ef4bd58cbcd0a4a662550305a09a7e2
David Jensenius <david@jensenius.com>
2020-07-26 22:27:47 -0400

    Setup webpack and other things

---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+const fs = require('fs');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveAppPath = relativePath => path.resolve(appDirectory, relativePath);
+// Host
+const host = process.env.HOST || 'localhost';
+
+// Required for babel-preset-react-app
+process.env.NODE_ENV = 'development';
+
+module.exports = {
+  entry: './src/index.tsx',
+  devtool: 'source-map',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svg-url-loader',
+            options: {
+              limit: 10000,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'condo-web.js',
+    path: path.resolve(__dirname, 'build'),
+    library: 'CondoWeb',
+  },
+  plugins: [
+    // Re-generate index.html with injected script tag.
+    // The injected script tag contains a src value of the
+    // filename output defined above.
+    new HtmlWebpackPlugin({
+      inject: 'body',
+      template: resolveAppPath('public/index.html'),
+    }),
+  ],
+  devServer: {
+    // Serve index.html as the base
+    contentBase: resolveAppPath('public'),
+    // Enable compression
+    compress: true,
+    // Enable hot reloading
+    hot: true,
+    host,
+    port: 3000,
+    // Public path is root of content base
+    publicPath: '/',
+  },
+};
