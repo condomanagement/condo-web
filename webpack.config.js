806bbae5dcd8f3d7af8d4991c0e64499b494dffa
David Jensenius <david@jensenius.com>
2020-07-27 21:16:17 -0400

    Add build tsconfig

--- webpack.config.js
+++ webpack.config.js
@@ -18,8 +18,11 @@ module.exports = {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        loader: 'ts-loader',
         exclude: /node_modules/,
+        options: {
+          configFile: 'tsconfig.build.json',
+        },
       },
       {
         test: /\.scss$/,
