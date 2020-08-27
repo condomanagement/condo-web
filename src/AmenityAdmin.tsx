87407c5984c2cc6619e12a1ca9f80ca5e7744106
David Jensenius <david@jensenius.com>
2020-08-27 19:13:47 -0400

    Indicate if users are active or not

--- src/AmenityAdmin.tsx
+++ src/AmenityAdmin.tsx
@@ -10,8 +10,14 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import TextField from '@material-ui/core/TextField';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogActions from '@material-ui/core/DialogActions';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
 
 type AmenityProp = {
   children: Amenity;
@@ -59,6 +65,8 @@ export default function AmenityAdmin(): JSX.Element {
   const [amenities, setAmenities] = useState<Amenity[]>([]);
   const [value, setValue] = useState('');
   const [timeLimit, setTimeLimit] = useState(60);
+  const [selectedAmenity, setSelectedAmenity] = useState<Amenity | undefined>(undefined);
+  const [amenityOpen, setAmenityOpen] = useState(false);
 
   const admin = new AdminManager();
   if (!admin) { return (<div />); }
@@ -87,6 +95,17 @@ export default function AmenityAdmin(): JSX.Element {
       });
   }
 
+  function editAmenity(amenity: Amenity): void {
+    setSelectedAmenity(amenity);
+    setTimeLimit(amenity.timeLimit);
+    setValue(amenity.name);
+    setAmenityOpen(true);
+  }
+
+  function updateAmenity(amenity?: Amenity): void {
+    debugger;
+  }
+
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     setValue(event.target.value);
   };
@@ -107,9 +126,14 @@ export default function AmenityAdmin(): JSX.Element {
           secondary={secondary}
         />
         <ListItemSecondaryAction>
-          <IconButton edge="end" aria-label="delete" onClick={(): void => { deleteAmenity(amenity.id); }}>
-            <DeleteIcon />
-          </IconButton>
+          <>
+            <IconButton edge="end" aria-label="edit" onClick={(): void => { editAmenity(amenity); }}>
+              <EditIcon />
+            </IconButton>
+            <IconButton edge="end" aria-label="delete" onClick={(): void => { deleteAmenity(amenity.id); }}>
+              <DeleteIcon />
+            </IconButton>
+          </>
         </ListItemSecondaryAction>
       </ListItem>
     );
@@ -127,47 +151,81 @@ export default function AmenityAdmin(): JSX.Element {
   };
 
   return (
-    <form className={classes.root} noValidate autoComplete="off" onSubmit={addAmenity}>
-      <h4 className="center">Admin</h4>
+    <>
+      <h4 className="center">Amenity Admin</h4>
       <div className="section flex-grow">
         <Grid container spacing={5}>
-          <Grid item xs={6}>
-            <TextField
-              id="standard-multiline-flexible"
-              label="Enter new amenity"
-              multiline
-              rowsMax={4}
-              value={value}
-              onChange={handleChange}
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <InputLabel htmlFor="age-native-simple">Time Limit</InputLabel>
-            <Select
-              native
-              value={timeLimit}
-              onChange={handleTimeLimitChange}
-              inputProps={{
-                name: 'timeLimit',
-                id: 'timeLimit',
-              }}
-              style={{ width: '100%' }}
-            >
-              {times}
-            </Select>
-          </Grid>
-          <Grid item xs={12}>
-            <Button className={classes.registerButton} variant="contained" type="submit">
-              Add Amenity
-            </Button>
-          </Grid>
           <Grid item xs={12}>
             <List>
               {amenities.map((amenity) => <AmenityLI key={amenity.id}>{amenity}</AmenityLI>)}
             </List>
           </Grid>
+          <Grid item xs={12}>
+            <Button
+              className={classes.registerButton}
+              variant="contained"
+              onClick={(): void => {
+                setTimeLimit(60);
+                setValue('');
+                setSelectedAmenity(undefined);
+                setAmenityOpen(true);
+              }}
+            >
+              Add Amenity
+            </Button>
+          </Grid>
         </Grid>
       </div>
-    </form>
+      <form className={classes.root} noValidate autoComplete="off" onSubmit={addAmenity}>
+        <Dialog open={amenityOpen} aria-labelledby="form-dialog-title">
+          <DialogTitle id="form-dialog-title">Edit Amenity</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              {selectedAmenity?.name}
+            </DialogContentText>
+            <Grid container spacing={5}>
+              <Grid item xs={12}>
+                <TextField
+                  id="standard-multiline-flexible"
+                  label="Enter new amenity"
+                  multiline
+                  rowsMax={4}
+                  value={value}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <InputLabel htmlFor="age-native-simple">Time Limit</InputLabel>
+                <Select
+                  native
+                  value={timeLimit}
+                  onChange={handleTimeLimitChange}
+                  inputProps={{
+                    name: 'timeLimit',
+                    id: 'timeLimit',
+                  }}
+                  style={{ width: '100%' }}
+                >
+                  {times}
+                </Select>
+              </Grid>
+              <Grid item xs={12}>
+                <Button className={classes.registerButton} variant="contained" type="submit">
+                  Add Amenity
+                </Button>
+              </Grid>
+            </Grid>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={(): void => setAmenityOpen(false)} color="secondary">
+              Cancel
+            </Button>
+            <Button onClick={(): void => updateAmenity(selectedAmenity)} color="primary">
+              Save
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </form>
+    </>
   );
 }
