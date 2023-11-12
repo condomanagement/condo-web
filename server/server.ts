dd3488db902248add9ca1b79fb99c8bec2c7f4d2
David Jensenius <david@jensenius.com>
2023-11-12 11:13:25 -0500

    Add server

---
+++ server/server.ts
@@ -0,0 +1,60 @@
+import express, { Express } from 'express';
+import rateLimit from 'express-rate-limit';
+import nocache from 'nocache';
+import cors, { CorsOptions } from 'cors';
+import proxy from 'express-http-proxy';
+import notFoundHandler from './middleware/not-found.middleware';
+
+const port = process.env.PORT || 8080;
+
+async function createServer(): Promise<Express> {
+  const app: Express = express();
+
+  const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    standardHeaders: true,
+    legacyHeaders: false,
+  });
+
+  app.use(
+    limiter,
+    nocache(),
+    express.urlencoded({ extended: true }),
+  );
+  const allowedOrigins = [
+    'http://localhost:8000',
+    'https://arrowlofts.org',
+    'https://www.arrowlofts.org',
+    'https://condo-web.azurewebsites.net',
+  ];
+
+  const corsOptions: CorsOptions = {
+    allowedHeaders: ['Authorization', 'Content-Type'],
+    origin: (origin, callback) => {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = 'The CORS policy for this site does not '
+                    + 'allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  };
+
+
+  app.use('/', cors(corsOptions), express.static('build'));
+  app.use('/api', cors(corsOptions), proxy('https://condo-web.azurewebsites.net'));
+
+  app.use(notFoundHandler);
+
+  return app;
+}
+
+createServer().then((app) => {
+  app.listen(port, () => {
+    console.warn(`⚡️[server]: Server is running at https://localhost:${port}`);
+  });
+});
